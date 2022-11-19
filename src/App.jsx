b8df9063f61bff1a0dfb242b443156c7b66ea06d
cahyadi16 <cahyaditkj626@gmail.com>
2022-11-19 21:12:05 +0700

    menambahkan function clsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import {
   IconBrandGithub,
   IconBrandTwitter,
 } from "@tabler/icons";
+import clsx from "clsx";
 
 export default function App() {
+  const type = "submit";
+  const onClick = () => console.log("login with another style");
   return (
     <div className={"bg-slate-900 grid place-content-center min-h-screen"}>
       <div className={"flex gap-x-2"}>
-        <Button
-          onClick={() => console.log("Login biasa")}
-          type="submit"
-          text="Login"
-        />
+        <Button {...{ type, onClick }}>Login biasa</Button>
         <Button
           className={"bg-blue-600"}
           onClick={() => console.log("Login with facebook")}
@@ -21,14 +20,17 @@ export default function App() {
           <IconBrandFacebook />
           Login
         </Button>
-        <Button className={"bg-blue-900"} type="button">
+        <Button
+          className={"bg-blue-900"}
+          onClick={() => console.log("Login with Twitter ")}
+          type="button"
+        >
           <IconBrandTwitter />
           Login
         </Button>
         <Button
           className={"bg-black"}
           onClick={() => console.log("Login with github")}
-          type="submit"
         >
           <IconBrandGithub />
           Login
@@ -39,16 +41,18 @@ export default function App() {
 }
 
 function Button(props) {
-  // pada clasname dibautkan default parameter untuk mengisi warna
-  const { className = "bg-blue-600", children, text } = props;
+  // pada clasname dibautkan default parameter untuk mengisi warna, dan mengisi default type
+  const { className = "bg-blue-600", children, text, type = "submit" } = props;
   return (
     <button
-      {...props} //untuk membuat fungsi bawaan dari button dapat berfungsi, seperti onClick, onChange, Type dll
-      className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1   flex items-center gap-x-2 text-white px-4 py-2 rounded`}
+      {...props}
+      type={type}
+      className={clsx(
+        className,
+        "[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1   flex items-center gap-x-2 text-white px-4 py-2 rounded"
+      )}
     >
       {text || children}
     </button>
   );
 }
-
-// lanjut ke menit ke 16
